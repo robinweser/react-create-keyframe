0dc56935503d3b51d5a6403044646414571a08f3
Robin Weser <contact@weser.io>
2024-06-15 14:55:07 +0200

    add hash comment

--- src/hash.ts
+++ src/hash.ts
@@ -1,12 +1,11 @@
+// copied from restyle
 export default function hash(str: string): string {
-  // FNV-1a Hash Function
   let h = 0 ^ 0x811c9dc5
   for (let index = 0; index < str.length; index++) {
     h ^= str.charCodeAt(index)
     h = (h * 0x01000193) >>> 0
   }
 
-  // Base36 Encoding Function
   const letters = 'abcdefghijklmnopqrstuvwxyz'
   const base36 = '0123456789' + letters
   let result = ''
